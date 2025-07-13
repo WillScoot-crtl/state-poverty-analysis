8bd4f64a2a3c72604231bbb807a1bda51764cc01
WillScoot-crtl <Webbwilliam06@gmail.com>
2025-07-13 12:13:38 -0500

    Add files via upload
    Updated code for state-poverty-analysis: the code can be imported across directories without running automatically, more discrete functions, a final data-frame at the end that includes the differing states, mean, and std, and I fixed a minor bug in how the mean and standard deviation was being calculated for each state; I didn't iterated over the data correctly, which resulted in each state having similar values.

--- poverty_data_analysis.py
+++ poverty_data_analysis.py
@@ -3,12 +3,13 @@ import pandas as pd
 
 # tracking historical data of an estimated percent of people
 # of all ages in poverty for various states
-def state_poverty_data():
+
+def data_links():
 
     root1 = 'https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23ebf3fb&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=866&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=PPAA'
     root2 = 'A156NCEN&scale=left&cosd=1989-01-01&coed=2023-01-01&line_color=%230073e6&link_values=false&line_style=solid&mark_type=none&mw=3&lw=3&ost=-99999&oet=99999&mma=0&fml=a&fq=Annual&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2025-07-06&revision_date=2025-07-06&nd=1989-01-01'
+                            
     
-    dict_historical_data = {}
     poverty_states = {'California': 'CA06000',
                       'Pennsylvania': 'PA42000',
                       'North Carolina': 'NC37000',
@@ -18,7 +19,15 @@ def state_poverty_data():
                       'New Jersey': 'NJ34000',
                       'North Dakota': 'ND38000',
                 }
+    
+    return poverty_states, root1, root2
+    
 
+
+def poverty_analysis():
+    
+    dict_historical_data = {}
+    poverty_states, root1, root2 = data_links()
 
     for state in poverty_states:
         state_data = []
@@ -27,30 +36,65 @@ def state_poverty_data():
 
         #renaming headers
         df_poverty.rename(columns={'observation_date': 'Observation Date',
-                                   f'PPAA{poverty_states[state]}A156NCEN': 'Annual Percentage'
-                          }, inplace=True)
+                                    f'PPAA{poverty_states[state]}A156NCEN': 'Annual Percentage'
+                                    }, inplace=True)
         df_poverty.rename(index={0: f'{state}'}, inplace=True)
 
 
-        
+                                    
 
         # concat individual keys and reassign to historical data
         df_concat = pd.concat(state_data)
         dict_historical_data[state] = df_concat
 
+    return df_poverty, df_concat, dict_historical_data
+
+
+
+    
+
+poverty_analysis()
+
+        
+
+def main_analysis():
+
+    df_poverty, df_concat, dict_historical_data = poverty_analysis()
+
+
+    states = []
+    mean   = []
+    std    = []
+    
+    dict_data = {'States': states, 'Mean': mean, 'Standard Deviation': std}
 
     for state in dict_historical_data:
-        data = []
+
+        df = dict_historical_data[state]
         print(f'{dict_historical_data[state]}\n')
-
+        
         #for each state, printing sum, mead, and standard deviation
-        data.append(df_poverty['Annual Percentage'].mean())
-        data.append(df_poverty['Annual Percentage'].std())
+        states.append(state)
+        mean.append(df['Annual Percentage'].mean())
+        std.append(df['Annual Percentage'].std())
 
-        df = pd.DataFrame(data, index=['Mean','Std'], columns=[f'{state}'])
-        print(df)
+       
+    df = pd.DataFrame(dict_data)
 
-state_poverty_data()
+    
+    print(df)
+        
+    
+
+if __name__ == "__main__":
+
+    main_analysis()
+
+
+
+
+
+
     
 
             
